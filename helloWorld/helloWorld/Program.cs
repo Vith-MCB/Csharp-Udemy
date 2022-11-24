fa8081c4a56b85607a99775484b3a1d46f3b3914
Vith-MCB <vitorriblacerda@gmail.com>
2022-11-24 15:53:34 -0300

    first steps

--- helloWorld/helloWorld/Program.cs
+++ helloWorld/helloWorld/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string name = "Vitor"; //Here the user input it name
+Console.WriteLine("Hello, "+ name);
+
+//Data types
+
+//Integers
+sbyte ex1 = 120; // -128 up to 127
+short ex2 = 200; // -32767 up to 32767
+int   ex3 = 200; // -2147483648 up to 2147483647
+long  ex4 = 212; // -9...bil up to 9...bil
+
+//Float
+float   ex1 = 9.9f; // 7-digit precision
+double  ex2 = 5.1;  // 15-digit precision
+decimal ex3 = 1.5;  // 28-digit precision
+
+//Logic
+bool switch = false; //True or False
+
+//Char and Strings
+char   ex1 = 'a';    //Single char or unicode
+string ex2 = "abcd"; //Multiple letters and unicodes
