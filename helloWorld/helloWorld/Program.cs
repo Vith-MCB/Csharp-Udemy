a31d11fc699637f8fcfa098bc3bed5c21af939eb
Vith-MCB <vitorriblacerda@gmail.com>
2022-11-24 21:18:02 -0300

    hello word and basics

--- helloWorld/helloWorld/Program.cs
+++ helloWorld/helloWorld/Program.cs
@@ -1,22 +1,25 @@
-﻿string name = "Vitor"; //Here the user input it name
-Console.WriteLine("Hello, "+ name);
+﻿Console.WriteLine("Enter your name: ");
+string name = Console.ReadLine(); //Here the user type his name (at console)
+Console.WriteLine("Hello, {0}", name); //Printing message using the user name
 
+/*
 //Data types
 
 //Integers
 sbyte ex1 = 120; // -128 up to 127
 short ex2 = 200; // -32767 up to 32767
-int   ex3 = 200; // -2147483648 up to 2147483647
-long  ex4 = 212; // -9...bil up to 9...bil
+int ex3 = 200; // -2147483648 up to 2147483647
+long ex4 = 212; // -9...bil up to 9...bil
 
 //Float
-float   ex1 = 9.9f; // 7-digit precision
-double  ex2 = 5.1;  // 15-digit precision
+float ex1 = 9.9f; // 7-digit precision
+double ex2 = 5.1;  // 15-digit precision
 decimal ex3 = 1.5;  // 28-digit precision
 
 //Logic
 bool switch = false; //True or False
 
 //Char and Strings
-char   ex1 = 'a';    //Single char or unicode
+char ex1 = 'a';    //Single char or unicode
 string ex2 = "abcd"; //Multiple letters and unicodes
+*/
