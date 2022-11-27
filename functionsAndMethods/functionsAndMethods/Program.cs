75eb14eb06574d44eade07af07670be7982dbb92
Vith-MCB <vitorriblacerda@gmail.com>
2022-11-27 18:09:35 -0300

    methods and functions

---
+++ functionsAndMethods/functionsAndMethods/Program.cs
@@ -0,0 +1,36 @@
+﻿/* 
+ * Methods examples
+ * 
+static int Add(int num1, int num2)
+{
+    return num1 + num2;
+}
+
+static int Multiply(int num1, int num2) 
+{ 
+    return num1 * num2; 
+}
+
+static double Divide(double num1, double num2) 
+{ 
+    return num1 / num2; 
+}
+
+*/
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+
+/*
+ * Methods Challenge
+ 
+
+string friend1 = "Guto", friend2 = "Jão", friend3 = "Juninho";
+static void GreetFriend(string friendName) {
+    Console.WriteLine("Hi {0}, my friend!",friendName);
+}
+GreetFriend(friend3);
+
+*/
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+
