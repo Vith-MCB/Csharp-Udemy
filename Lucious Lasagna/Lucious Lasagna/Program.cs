d6358102d5567e72aceb1cbf528f18aa54c7339d
Vith-MCB <vitorriblacerda@gmail.com>
2022-11-27 23:13:47 -0300

    Method projects

---
+++ Lucious Lasagna/Lucious Lasagna/Program.cs
@@ -0,0 +1,26 @@
+﻿//This problem is used to learn methods concept
+
+//Lasagna class
+public class Lasagna {
+    int timeInOven = 40; //Time in minutes that the lasagna should be in the oven
+
+    public static void Main(string[] args)
+    {
+        Lasagna lasagna = new Lasagna(); //creating new lasagna
+
+        int tempo = 
+        Console.WriteLine(tempo);
+    }
+
+    /// <summary>
+    /// 1) Defining the expect oven time in minutes
+    /// </summary>
+    /// <returns></returns>
+    public int expectedMinutesInOven(Lasagna lasagna)
+    {
+        return lasagna.timeInOven;
+    }
+}
+
+
+
