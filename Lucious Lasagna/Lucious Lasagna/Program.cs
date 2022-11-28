98eb0fc9aec2e973f128d526ab092860f30906dd
Vith-MCB <vitorriblacerda@gmail.com>
2022-11-28 11:52:05 -0300

    Functions and finish challenge

--- Lucious Lasagna/Lucious Lasagna/Program.cs
+++ Lucious Lasagna/Lucious Lasagna/Program.cs
@@ -8,19 +8,47 @@ public class Lasagna {
     {
         Lasagna lasagna = new Lasagna(); //creating new lasagna
 
-        int tempo = 
-        Console.WriteLine(tempo);
+        Console.WriteLine("Lasagna needs to stay {0} minutes in the oven.", lasagna.expectedMinutesInOven());
+        Console.WriteLine("Remaining time in oven: {0} minutes.", lasagna.remainingMinutesInOven(20));
+        Console.WriteLine("Time spent preparing: {0} minutes.", lasagna.preparationTimeInMinutes(2));
+        Console.WriteLine("Time spent: {0} minutes.", lasagna.elapsedTimeInMinutes(3, 20));
+
     }
 
     /// <summary>
-    /// 1) Defining the expect oven time in minutes
+    /// 1) Defining the expect oven time in minutes.
     /// </summary>
-    /// <returns></returns>
-    public int expectedMinutesInOven(Lasagna lasagna)
+    /// <returns>Time that lasagna needs to stay in oven.</returns>
+    public int expectedMinutesInOven()
     {
-        return lasagna.timeInOven;
+        return timeInOven;
     }
+
+    /// <summary>
+    /// 1) Calculates the remaining oven time in minutes.
+    /// </summary>
+    /// <returns>Remaining time that lasagna needs to stay in oven.</returns>
+    public int remainingMinutesInOven(int minutesInOven)
+    {
+        return timeInOven - minutesInOven;
+    }
+
+    /// <summary>
+    /// 1) Calculates the preparation time in minutes.
+    /// </summary>
+    /// <returns>Returns how many minutes you spent prepairing the lasagna, assuming each layer takes you 2 minutes to prepare.</returns>
+    public int preparationTimeInMinutes(int numberOfLayers)
+    {
+        return numberOfLayers * 2;
+    }
+
+    /// <summary>
+    /// 1) Calculates the preparation time in minutes.
+    /// </summary>
+    /// <returns>Returns how many minutes you spent prepairing the lasagna, assuming each layer takes you 2 minutes to prepare.</returns>
+    public int elapsedTimeInMinutes(int numberOfLayers, int minutesInOven)
+    {
+        return (numberOfLayers * 2) + minutesInOven;
+    }
+
 }
-
-
-
--- functionsAndMethods/functionsAndMethods/Program.cs
+++ functionsAndMethods/functionsAndMethods/Program.cs
@@ -34,3 +34,33 @@ GreetFriend(friend3);
 
 //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
 
+using System;
+
+namespace Coding.Exercise
+{
+    public class Method
+    {
+        // Place for your methods
+
+
+        string LowUpper(string s)
+        {
+            string sumString = s.ToLower + s.ToUpper;
+            return sumString;
+        }
+
+        public static void Run()
+        {
+            // We encourage you to test your code with different strings,
+            // but don't forget to put the default string back at the end of your testing.
+            string s = "HeY ThErE !";
+
+            /// Change nothing down here 
+            s = LowUpper(s);
+            Console.WriteLine(s);
+            Count(s);
+
+        }
+    }
+}
+
