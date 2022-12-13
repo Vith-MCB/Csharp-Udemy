541630f2eecd4ab15b559b1549f9f2dfbd11ddbf
Vith-MCB <vitorriblacerda@gmail.com>
2022-12-12 23:08:08 -0300

    Classes

---
+++ Classes/Classes/Human.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    class Human
+    {
+        private string firstName;
+        private string lastName;
+        private int age;
+
+        //Default Constructor
+        public Human()
+        {
+            Console.WriteLine("Constructor called. Object Created");
+        }
+
+
+
+        //Parameterized constructor
+        public Human(string firstName, string lastName, int age)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.age = age;
+        }
+
+        public void IntroduceMyself()
+        {
+            Console.WriteLine("Hi, my name is {0} {1}, nice to meet you! I'm {2}yo.", firstName, lastName, age);
+        }
+    }
+}
---
+++ Classes/Classes/Program.cs
@@ -0,0 +1,20 @@
+﻿//Crating object of my class
+
+using Classes;
+
+//Instance of Human Class
+Human person1 = new Human("Vitor", "Ribeiro", 20); //Using parameterized constructor
+
+
+//The variables below doesn't need to be used anymore, becouse of the constructor of the Human Class
+//person.firstName = "Vitor";
+//person.lastName = "Ribeiro";
+
+person1.IntroduceMyself();
+
+
+Human person2 = new Human("Luiz", "Henrique", 17); //Using parameterized constructor
+person2.IntroduceMyself();
+
+Human basicHuman = new Human(); //Using basic constructor
+basicHuman.IntroduceMyself();
