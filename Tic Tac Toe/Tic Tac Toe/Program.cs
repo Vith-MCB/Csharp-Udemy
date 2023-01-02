1c45c137ab2def3b98a7c7b3a48cbfaeaa9eb9cd
Vith-MCB <vitorriblacerda@gmail.com>
2023-01-02 13:36:02 -0300

    function used to check tiktaktoe

--- Tic Tac Toe/Tic Tac Toe/Program.cs
+++ Tic Tac Toe/Tic Tac Toe/Program.cs
@@ -1,1 +1,32 @@
-﻿
+﻿using System;
+
+namespace Coding.Exercise
+{
+    public class TicTacToe
+    {
+        public static bool Checker(string[,] board)
+        {
+            //Checking diagonals
+            if (board[0, 0].Equals(board[1, 1]) && board[0, 0].Equals(board[2, 2]))
+            {
+                return true;
+            }
+            if (board[2, 0].Equals(board[1, 1]) && board[2, 0].Equals(board[0, 2]))
+            {
+                return true;
+            }
+
+            //Checking horizontaly
+            if (board[0, 0].Equals(board[0, 1]) && board[0, 0].Equals(board[0, 2])) { return true; }
+            if (board[1, 0].Equals(board[1, 1]) && board[1, 0].Equals(board[1, 2])) { return true; }
+            if (board[2, 0].Equals(board[2, 1]) && board[2, 0].Equals(board[2, 2])) { return true; }
+
+            //Checking verticaly
+            if (board[0, 0].Equals(board[1, 0]) && board[0, 0].Equals(board[2, 0])){ return true; }
+            if (board[0, 1].Equals(board[1, 1]) && board[0, 1].Equals(board[2, 1])){ return true; }
+            if (board[0, 2].Equals(board[1, 2]) && board[0, 2].Equals(board[2, 2])){ return true; }
+
+            return false;
+        }
+    }
+}
