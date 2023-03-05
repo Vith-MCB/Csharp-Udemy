8b0557dce2138ddca3e0cb8fa6c85bf6bc74d0db
Vitor Ribeiro <vitorriblacerda@gmail.com>
2023-03-04 22:27:17 -0300

    Changes at inheritance code

--- Rider IDE/Inheritance DEMO - Posts/Inheritance DEMO - Posts/Program.cs
+++ Rider IDE/Inheritance DEMO - Posts/Inheritance DEMO - Posts/Program.cs
@@ -12,6 +12,10 @@ namespace Inheritance_DEMO___Posts
             ImagePost imagePost1 = new ImagePost("Look, the sky is beautiful!", "Vitor Ribeiro",
                 "https://images.alphacoders.com/123/1230061.jpg", true);
             Console.WriteLine(imagePost1.ToString());
+
+            VideoPost videoPost = new VideoPost("Deftones - Sextape", "Deftones",
+                "https://www.youtube.com/watch?v=f0pdwd0miqs&ab_channel=Deftones", false);
+            Console.WriteLine(videoPost.ToString());
         }
     }
 }
---
+++ Rider IDE/Inheritance DEMO - Posts/Inheritance DEMO - Posts/VideoPost.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Inheritance_DEMO___Posts
+{
+    public class VideoPost : Post
+    {
+        public string VideoURL { get; set; }
+
+
+
+        //Generic Constructor
+        public VideoPost()
+        {
+            Id = 0;
+            Title = "This is a generic Video!";
+            IsPublic = false;
+            SendByUsername = "YouTube";
+            VideoURL = "https://youtube.com/";
+        }
+
+        public VideoPost(string title, string sendByUsername,
+            string videoUrl, bool isPublic)
+        {
+            this.Id = GetNextID();
+            this.Title = title;
+            this.SendByUsername = sendByUsername;
+            this.IsPublic = isPublic;
+
+            //Image URL is a member of Image Post, but not from post
+            this.VideoURL = videoUrl;
+        }
+
+        public override string ToString()
+        {
+            if (IsPublic)
+            {
+                return String.Format("{0} - {1} - by {2} - Link: {3}", this.Id,
+                    this.Title, this.SendByUsername, this.VideoURL);
+            }
+            else
+            {
+                return String.Format("{0} IS PRIVATE VIDEO!",this.Title);
+            }
+        }
+    }
+}
