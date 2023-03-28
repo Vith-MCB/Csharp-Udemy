22bdf9a27ffb2f51c3b78b18818a4d380972e83f
Vitor Ribeiro <vitorriblacerda@gmail.com>
2023-03-27 21:39:51 -0300

    IEnumerable

---
+++ Rider IDE/IEnumerableDemo/IEnumerableDemo/Program.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+// 1. IEnumerable <T> for generic collections
+// 2. IEnumerable for non generic collections
+
+DogShelter shelter = new DogShelter();
+foreach (Dog dog in shelter)
+{
+    if (!dog.IsNaughtyDog)
+    {
+        dog.GiveTreat(2);
+    }
+    else
+    {
+        dog.GiveTreat(0);
+    }
+}
+
+
+class Dog
+{
+    public string Name { get; set; }
+    
+    public bool IsNaughtyDog { get; set; }
+
+    public Dog(string name, bool isNaughty)
+    {
+        this.Name = name;
+        this.IsNaughtyDog = isNaughty;
+    }
+
+    public void GiveTreat(int numberOfTreats)
+    {
+        Console.WriteLine("Dog: {0} said woof {1} times!", Name,numberOfTreats);
+    }
+}
+
+class DogShelter : IEnumerable<Dog>
+{
+    public List<Dog> dogs;
+
+    public DogShelter()
+    {
+        dogs = new List<Dog>()
+        {
+            new Dog("David", false),
+            new Dog("Michael", true),
+            new Dog("Sif", false)
+        };
+    }
+
+    public IEnumerator<Dog> GetEnumerator()
+    {
+        return dogs.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+}
