807893a1f0463c3f0c9c5bfc6c23ddcf011416cd
Vitor Ribeiro <vitorriblacerda@gmail.com>
2023-04-17 08:25:38 -0300

    IEnumerableDemo2

---
+++ Rider IDE/IEnumerableDEMO2/IEnumerableDEMO2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+
+//Creating a generic variable that will be used to store collections
+IEnumerable<int> unknownCollection = GetCollection(3);
+
+//Printing the numbers inside of the variable
+foreach (int num in unknownCollection)
+{
+    Console.Write(num + " ");
+}
+
+static IEnumerable<int> GetCollection(int option)
+{
+    //Creating a List of type integer
+    List<int> numbersList = new List<int>() { 1, 2, 3, 4, 5 };
+
+    //Creating a Queue of type integer
+    Queue<int> numbersQueue = new Queue<int>();
+    //Add values to the queue
+    numbersQueue.Enqueue(6);
+    numbersQueue.Enqueue(7);
+    numbersQueue.Enqueue(8);
+    numbersQueue.Enqueue(9);
+    numbersQueue.Enqueue(10);
+
+    if (option == 1)
+    {
+        return numbersList;
+    }
+    else if (option == 2)
+    {
+        return numbersQueue;
+    }
+    else
+    {
+        return new int[] { 11, 12, 13, 14, 15 };
+    }
+}
