a782dfde4898e99e26a67742d3a1cddb62646ce6
Vitor Ribeiro <vitorriblacerda@gmail.com>
2023-04-17 08:49:40 -0300

    IEnumerable sum demo

---
+++ Rider IDE/IEnumerableDEMO3/IEnumerableDEMO3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+List<int> numberList = new List<int>() { 8, 7, 5 };
+
+int[] numberArray = new int[] { 1, 2, 3, 4 };
+
+CollectionsSum(numberList);
+CollectionsSum(numberArray);
+
+static void CollectionsSum(IEnumerable<int> anyCollection)
+{
+    int sum = 0;
+
+    foreach (int num in anyCollection)
+    {
+        sum += num;
+    }
+
+    Console.WriteLine("Sum is: {0}", sum);
+}
