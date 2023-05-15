ec206aa167d0fe8c8df14f0ef8a033ae07948df9
Vitor Ribeiro <vitorriblacerda@gmail.com>
2023-05-15 16:54:49 -0300

    challange completed

---
+++ Rider IDE/FileChallenge/FileChallenge/Program.cs
@@ -0,0 +1,45 @@
+﻿#region Variables and constants
+
+const string filepath =
+    @"D:\Programação\GitHub\Csharp-Udemy\Rider IDE\FileChallenge\FileChallenge\files\input.txt";
+
+const string outputfile = 
+    @"D:\Programação\GitHub\Csharp-Udemy\Rider IDE\FileChallenge\FileChallenge\files\output.txt";
+
+//Defining the text vector (Line by line)
+string[] lines = File.ReadAllLines(filepath);
+
+string result = "";
+    
+#endregion
+
+#region Main
+
+foreach (string str in lines)
+{
+    if (FindSplit(str))
+    {
+        string[] splitParts = str.Split(' ');
+        if (result == ""){result += splitParts[4];} 
+        else { result += " " + splitParts[4]; }
+        
+    }
+}
+
+File.WriteAllText(outputfile,result);
+
+#endregion
+
+
+#region Functions
+
+bool FindSplit(string input)
+{
+    var targetWord = "split";
+    var position = input.IndexOf(targetWord, StringComparison.OrdinalIgnoreCase);
+
+    if (position != -1) {return true;}
+    else { return false; }
+}
+
+#endregion
