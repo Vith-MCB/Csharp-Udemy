f1baa1055297497c07512f1fd8e70dd7cc0ac6f2
Vitor Ribeiro <vitorriblacerda@gmail.com>
2023-08-02 12:18:40 -0300

    Structures

---
+++ Rider IDE/Structs/Structs/Program.cs
@@ -0,0 +1,23 @@
+﻿//Structs
+//To use structs it must be initializated first, just like classes
+car car1;
+car1.name = "Gol";
+car1.company = "Volkswagen";
+car1.price = 52000;
+
+car1.DisplayInfo();
+
+//Structs DO NOT support inheritance
+struct car{
+    public string name;
+    public string company;
+    public double price;
+
+    public void DisplayInfo()
+    {
+        Console.WriteLine(name);
+        Console.WriteLine(company);
+        Console.WriteLine(price);
+    }
+}
+
