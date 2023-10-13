a28f3400a7a6da665e9e4ece083da234438bf085
Vitor Ribeiro <vitorriblacerda@gmail.com>
2023-10-13 12:32:57 -0300

    Delegates

---
+++ Rider IDE/Delegates/Delegates/Program.cs
@@ -0,0 +1,28 @@
+﻿//Using Delegates
+
+List<string> names = new List<string>() {"Aiden", "Sif", "Walter", "Anatoli", "Ion"};
+
+Console.WriteLine("----Before the Delegate----");
+foreach (string name in names)
+{
+    Console.WriteLine(name);
+}
+
+names.RemoveAll(Filter);
+
+static bool Filter(string str)
+{
+    //Return whether the string 'str' contains the letter 'i'
+    //The '.Contains' method will return a bool which will be return
+    string filterStr = "i";
+    return str.ToLower().Contains(filterStr);
+
+}
+
+Console.WriteLine("----After the Delegate----");
+foreach (string name in names)
+{
+    Console.WriteLine(name);
+}
+
+//Creating Delegates
