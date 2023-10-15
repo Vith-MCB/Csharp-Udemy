42fe5ecea6758c52e69e1da6a42e8ecdae8df3b1
Vitor Ribeiro <vitorriblacerda@gmail.com>
2023-10-15 13:21:09 -0300

    Lambda Func exercise

--- Rider IDE/AnonymousMethods and LambdaExpressions/AnonymousMethods and LambdaExpressions/Program.cs
+++ Rider IDE/AnonymousMethods and LambdaExpressions/AnonymousMethods and LambdaExpressions/Program.cs
@@ -99,3 +99,68 @@ class Program
         Console.WriteLine("Sum of all numbers: " + sum);
     }
 }
+
+/*
+Lambda expressions
+#########################################
+
+Please, avoid changing the source code for the exercise!
+
+(Unless you want to import something)
+
+#########################################
+
+
+
+Hey there, it is going to be an advanced challenge related to Lambda expressions!
+
+
+
+Subject:
+
+Create a dictionary that will contain <string, Func<>> pairs. The keys will be operation signs "+", "-", "/", "*" and values will be the lambda expressions that will perform the corresponding operation on two floats and return a result with type float.
+
+
+
+One more time in detail:
+
+Create and store using static keyword Lambda expressions with names Plus, Minus, Divide and Multiply.
+
+You can store function as follow:
+
+static Func<int, int> Name = (a) => a;
+
+Create a static dictionary called Operators that uses a string as a Key and lambda function as a value.
+
+Example: {"sign", Function}
+
+Create a static method OperationGet that will get as input a string, test if this string is a Key in the dictionary and if positive return function otherwise returns null.
+
+*/
+
+using System;
+using System.Collections.Generic;
+
+namespace Coding.Exercise
+{
+    public class Run
+    {
+        static Func<float, float, float> Plus = (a,b) => a + b;
+        static Func<float, float, float> Minus = (a,b) => a - b;
+        static Func<float, float, float> Multiply = (a,b) => a * b;
+        static Func<float, float, float> Divide = (a,b) => a / b;
+            
+        static Dictionary<string, Func<float,float,float>> Operators = new Dictionary<string, Func<float,float,float>>{
+            { "+", Plus },
+            { "-", Minus },
+            { "/", Divide },
+            { "*", Multiply }
+        };
+
+        public static Func<float, float, float> OperationGet(string operatorSign){
+            if (Operators.ContainsKey(operatorSign)){return Operators[operatorSign];}
+            else{return null;}
+        }
+    }
+}
+
